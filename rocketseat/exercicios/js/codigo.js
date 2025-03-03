c54c5212f6dc1635513890e5603a16245e21ec41
CharlesThomaz <charlesthomazdr@gmail.com>
2025-03-03 15:12:03 -0300

    update imgs, css, novos arquivos

---
+++ rocketseat/exercicios/js/codigo.js
@@ -0,0 +1,6 @@
+let produto = document.querySelector(".produto");
+let idProduto = produto.dataset.id; // "123"
+let precoProduto = produto.dataset.preco; // "99.90"
+
+console.log(idProduto)
+console.log(precoProduto)
